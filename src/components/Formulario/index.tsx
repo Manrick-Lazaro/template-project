6ccb28e05fc30e2ac909958fd9fb98716ef59d8b
Israel Kleber <israel.lima@escolar.ifrn.edu.br>
2024-03-19 15:50:16 -0300

    feat: adiciona resolução da atividade da aula 5

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,59 +1,79 @@
 import { useForm } from "react-hook-form";
 
-export default function Form() {
-	const { register, handleSubmit, getValues, reset } = useForm();
+export default function Form(): JSX.Element {
+	const { getValues, reset, handleSubmit, register } = useForm();
 
-	function onSubmit(data: any): void {
-		console.log(data); // Aqui estão os dados do formulário
-	}
-
-	function mostrarValor(): void {
-		const { nome, email } = getValues();
-
-		// Realize a validação dinâmica com os valores dos campos
-		// Exemplo de validação simples
-		console.log("valor de nome: ", nome);
-		console.log("valor de email: ", email);
-	}
-
-	function limparValor(): void {
-		console.log("Antes de limpar", getValues());
+	function resetValues(): void {
 		reset();
-		console.log("Depois de limpar", getValues());
+	}
+
+	function onSubmit(): void {
+		const { nome, email, telefone, idade } = getValues();
+
+		if (nome != "" && email != "" && telefone != "" && idade != "") {
+			console.log("ebaaaa tudo certo com os dados!");
+		} else {
+			console.log("deu errado aí, faltou preencher algum dado!");
+		}
 	}
 
 	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<label>
-				Nome:
-				<input
-					type="text"
-					{...register("nome")}
-				/>
-			</label>
-			<br />
-			<label>
-				E-mail:
-				<input
-					type="email"
-					{...register("email")}
-				/>
-			</label>
-			<br />
+		<>
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className="d-flex flex-column rounder-sm shadow-sm container-md"
+			>
+				<section className="d-flex flex-column mt-5">
+					<label className="d-flex flex-column">
+						Nome:
+						<input
+							type="text"
+							className="p-1 border-gray-20 rounder-sm border-solid-md"
+							{...register("nome")}
+						/>
+					</label>
+					<label className="d-flex flex-column mt-3">
+						E-mail:
+						<input
+							type="email"
+							className="p-1 border-gray-20 rounder-sm border-solid-md"
+							{...register("email")}
+						/>
+					</label>
+					<label className="d-flex flex-column mt-3">
+						Telefone:
+						<input
+							type="text"
+							className="p-1 border-gray-20 rounder-sm border-solid-md"
+							{...register("telefone")}
+						/>
+					</label>
+					<label className="d-flex flex-column mt-3">
+						Idade:
+						<input
+							type="number"
+							className="p-1 border-gray-20 rounder-sm border-solid-md"
+							{...register("idade")}
+						/>
+					</label>
+				</section>
 
-			<button
-				type="button"
-				onClick={mostrarValor}
-			>
-				Mostrar valores
-			</button>
-			<button type="submit">Enviar</button>
-			<button
-				type="button"
-				onClick={limparValor}
-			>
-				Limpar valores
-			</button>
-		</form>
+				<div className="d-flex flex-row justify-content-center mt-5 mb-5">
+					<button
+						type="button"
+						onClick={resetValues}
+						className="br-button secondary mr-1"
+					>
+						Limpar valores
+					</button>
+					<button
+						type="submit"
+						className="br-button primary ml-1"
+					>
+						Enviar
+					</button>
+				</div>
+			</form>
+		</>
 	);
 }
